db1fa21e78fb808446b4a79d39dcc44ebbc5eb35
Natalia_K <natalia_natalia.1@mail.ru>
2021-05-14 11:38:36 +1000

    initial commit

--- tours/views.py
+++ tours/views.py
@@ -5,7 +5,7 @@ def main_view(request):
     return render(request, 'tours/index.html')
 
 def departure_view(request):
-    return render(request, 'departure.html')
+    return render(request, 'tours/departure.html')
 
 def tour_view(request):
-    return render(request, 'tour.html')
+    return render(request, 'tours/tour.html')
