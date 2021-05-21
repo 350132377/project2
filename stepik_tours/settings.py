537639538365b05028207018b931ee9e2657fa8e
Natalia_K <natalia_natalia.1@mail.ru>
2021-05-22 01:11:04 +1000

    initial commit

--- stepik_tours/settings.py
+++ stepik_tours/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'app/static'),
+    os.path.join(BASE_DIR, 'tours/static'),
 )
 
 # Default primary key field type
